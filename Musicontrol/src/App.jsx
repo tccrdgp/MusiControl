66676e6917da37b3f6adc5699c6c056a1fb930c3
tccrdgp <tcc.rdgp@gmail.com>
2025-08-20 23:29:20 -0300

    Update App.jsx

--- Musicontrol/src/App.jsx
+++ Musicontrol/src/App.jsx
@@ -4,7 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(2)
+  const [count, setCount] = useState(1)
 
   return (
     <>
@@ -18,7 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 3)}>
+        <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
